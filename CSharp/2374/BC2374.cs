d90988684d0794a9c3c8a24d8b7045b8da8b4133
Neiva04 <neivajoseph@gmail.com>
2023-02-24 16:00:01 -0300

    questão

---
+++ CSharp/2374/BC2374.cs
@@ -0,0 +1,17 @@
+﻿using System; 
+
+class URI{
+
+
+    static void Main(string[] args) { 
+        
+        int N = int.Parse(Console.ReadLine());
+        int M = int.Parse(Console.ReadLine());
+        int dif = N-M;
+
+        Console.WriteLine(dif);
+        
+    }
+
+
+}
